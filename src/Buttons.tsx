64ebd6b969fdd24b9f48182627494f99bff6adeb
Dawid Domiński <dawid.dominski1125@gmail.com>
2022-08-14 01:22:37 +0200

    Add graphic design

---
+++ src/Buttons.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const ButtonsContainer = () => {
+
+    return (
+        <div>
+            <div className="operators">
+                <button>(</button>
+                <button>)</button>
+                <button>^</button>
+                <button>Del</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button>/</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>*</button>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>-</button>
+                <button>0</button>
+                <button>.</button>
+                <button>=</button>
+                <button>+</button>
+
+
+            </div>
+        </div>
+    );
+};
+
+export default ButtonsContainer;
---
+++ src/Results.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ResultsContainer = () => {
+    return (
+        <div className="result">
+            <div className="lastAction">5</div>
+            <div className="action">20</div>
+        </div>
+    );
+};
+
+export default ResultsContainer;
